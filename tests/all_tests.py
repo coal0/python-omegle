52edcab404aa9a673b36459215ec73d1ee850ad7
Daniel <daniel_x3@protonmail.com>
2018-07-14 22:10:41 +0200

    Create all_tests.py

---
+++ tests/all_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from interests_chat_test import InterestsChatTest
+from random_chat_test import RandomChatTest
+from spyee_chat_test import SpyeeChatTest
+
+if __name__ == "__main__":
+    unittest.main()
